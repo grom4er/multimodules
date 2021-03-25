7de2efcce063c452c8c71407fb49b42f0e491c2e
grom4er@gmail.com <iloveyouGrom55>
2021-03-25 11:31:08 +0200

    Change variables

--- first-service/src/main/java/com/example/multimodule/controller/HelloFirstServiceController.java
+++ first-service/src/main/java/com/example/multimodule/controller/HelloFirstServiceController.java
@@ -1,14 +1,14 @@
 package com.example.multimodule.controller;
 
 import com.example.multimodule.client.FeignProxy;
-import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequiredArgsConstructor
 @RestController
 public class HelloFirstServiceController {
-    private final FeignProxy proxy;
+    @Setter
+    private FeignProxy proxy;
 
     @GetMapping("/")
     public String sayHi() {
@@ -17,6 +17,6 @@ public class HelloFirstServiceController {
 
     @GetMapping("/ex")
     public String exception() {
-       return proxy.exception("error"); // Если параметр будет null или error - второй сервис выдаст ошибку.
+        return proxy.exception("error"); // Если параметр будет null или error - второй сервис выдаст ошибку.
     }
 }
--- first-service/src/main/java/com/example/multimodule/controlleradvice/FeignExceptionHandler.java
+++ first-service/src/main/java/com/example/multimodule/controlleradvice/FeignExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.example.multimodule.advice;
+package com.example.multimodule.controlleradvice;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
