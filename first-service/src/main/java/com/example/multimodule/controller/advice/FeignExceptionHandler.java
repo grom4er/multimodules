207037b7c72a5f0a7bd1ccfc26851be168d68dbd
grom4er@gmail.com <iloveyouGrom55>
2021-03-25 12:32:25 +0200

    Add postman_collection.json

--- first-service/src/main/java/com/example/multimodule/controller/advice/FeignExceptionHandler.java
+++ first-service/src/main/java/com/example/multimodule/controller/advice/FeignExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.multimodule.controller.advice;
 
 import com.example.multimodule.exception.NotAuthorizedUserException;
 import com.example.multimodule.exception.WrongDataException;
+import feign.FeignException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,13 +15,13 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class FeignExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(value = {NotAuthorizedUserException.class})
-    protected ResponseEntity<Object> handleAuthorizedException(RuntimeException exception, WebRequest request) {
+    protected ResponseEntity<Object> handleAuthorizedException(FeignException exception, WebRequest request) {
         String response = "Authorized problem. Please check you data and try again";
         return handleExceptionInternal(exception, response, new HttpHeaders(), HttpStatus.UNAUTHORIZED, request);
     }
 
     @ExceptionHandler(value = {WrongDataException.class})
-    protected ResponseEntity<Object> handleWrongDataException(RuntimeException exception, WebRequest request) {
+    protected ResponseEntity<Object> handleWrongDataException(FeignException exception, WebRequest request) {
         String response = "Server take wrong data. Please, check data.";
         return handleExceptionInternal(exception, response, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
