3379f2e99a2da341dfe29ac1d5a1d609d35ad589
grom4er@gmail.com <iloveyouGrom55>
2021-03-25 11:31:08 +0200

    Change variables

--- first-service/src/main/java/com/example/multimodule/controller/HelloFirstServiceController.java
+++ first-service/src/main/java/com/example/multimodule/controller/HelloFirstServiceController.java
@@ -1,25 +1,22 @@
 package com.example.multimodule.controller;
 
 import com.example.multimodule.client.FeignProxy;
-import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequiredArgsConstructor
 @RestController
 public class HelloFirstServiceController {
-    private final FeignProxy proxy;
+    @Setter
+    private FeignProxy proxy;
 
     @GetMapping("/")
-    public String sayHi(@RequestHeader(value = "auth", required = false) String header) {
-        return "Hi. I am first controller. " + proxy.sayHi(header);
+    public String sayHi() {
+        return "Hi. I am first controller. " + proxy.sayHi();
     }
 
     @GetMapping("/ex")
-    public String exception(@RequestHeader(value = "auth", required = false) String header,
-                            @RequestParam(value = "msg", required = false) String msg) {
-        return proxy.exception(header, msg);
+    public String exception() {
+        return proxy.exception("error"); // Если параметр будет null или error - второй сервис выдаст ошибку.
     }
 }
